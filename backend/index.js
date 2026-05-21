33784d65d30139ca54579a45425986143a9e2eb1
Kelven Colombo <kelven_colombo@outlook.com>
2026-05-21 11:06:33 -0400

    refactor: reestruturar arquitetura para fullstack e inicializar frontend com vite

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { db, iniciarBanco } from "./database.js";
 import transacoesRoutes from"./transacoesRoutes.js";
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(transacoesRoutes);
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Caixa MC Serviços</h1>
+    </div>
+  )
+}
+
+export default App
